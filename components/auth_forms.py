b94e484b3c83393dfdf3a59e90febff77a56538e
Hamza Baig <baighamza02@gmail.com>
2024-10-31 17:45:39 +0500

    Trim email input

--- components/auth_forms.py
+++ components/auth_forms.py
@@ -2,13 +2,14 @@ import streamlit as st
 from utils.validation import is_valid_email
 
 def render_signup_form():
-    registration_data={}
+    registration_data = {}
     st.subheader("Create a New Account")
-    registration_data['email'] = st.text_input("Email", key="signup_email")
+    registration_data['email'] = st.text_input("Email", key="signup_email").strip()
     registration_data['password'] = st.text_input("Password", type="password", key="signup_password")
     registration_data['confirm_password'] = st.text_input("Confirm Password", type="password", key="signup_confirm_password")
 
     if st.button("Sign Up"):
+        registration_data['email'] = registration_data['email'].strip()
         if not is_valid_email(registration_data['email']):
             st.error("Email address is not valid")
         elif registration_data['password'] != registration_data['confirm_password']:
@@ -18,12 +19,13 @@ def render_signup_form():
 
 
 def render_login_form():
-    login_data={}
+    login_data = {}
     st.subheader("Login to Your Account")
-    login_data['email'] = st.text_input("Email", key="login_email")
+    login_data['email'] = st.text_input("Email", key="login_email").strip()
     login_data['password'] = st.text_input("Password", type="password", key="login_password")
 
     if st.button("Login"):
+        login_data['email'] = login_data['email'].strip()
         if not is_valid_email(login_data['email']):
             st.error("Email address is not valid")
         else:
